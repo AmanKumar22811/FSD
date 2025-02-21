8bd34bbe2b5f3b378a5603c04e50cc7f1fbb0a1b
Aman Kumar <aman228115918@gmail.com>
2025-02-21 09:55:49 +0530

    fsandhttp

--- Backend Work/httpModule/index.js
+++ Backend Work/httpModule/index.js
@@ -1,10 +1,27 @@
 const http = require("http");
 
 const PORT = 8080;
-const server = http.createServer((_, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h1 style='color:blue;background-color:yellow'>Hello</h1>");
-  res.end();
+const server = http.createServer((req, res) => {
+  if (req.url === "/" && req.method === "GET") {
+    res.setHeader("Content-Type", "text/html");
+    console.log(req.url + " " + req.method);
+    res.write("<h1 style='color:blue;background-color:yellow'>Hello</h1>");
+    res.end();
+  }
+  if (req.url === "/show" && req.method === "GET") {
+    res.end(
+      JSON.stringify({
+        message: "Successfully hit the api",
+      })
+    );
+  }
+  if (req.url === "/show" && req.method === "POST") {
+    res.end(
+      JSON.stringify({
+        message: "Successfully hit the post api @ /show",
+      })
+    );
+  }
 });
 
 server.listen(PORT, () => console.log(`Server listen at ${PORT}`));
